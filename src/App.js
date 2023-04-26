fc1cfa02047ebd72201d5902051c650641219d13
Chloe <chloe.boyd@outlook.com>
2023-04-26 12:36:54 -0700

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
-import CampsitesDirectoryPage from './pages/campsitesDirectoryPage';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
