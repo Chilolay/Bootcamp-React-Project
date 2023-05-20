908f5697f739e3159159eafcf84e249923254d58
Chloe <chloe.boyd@outlook.com>
2023-05-20 13:14:38 -0700

    Fixed error

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -65,7 +65,7 @@ const commentsSlice = createSlice({
         },
         [postComment.rejected]: (state, action) => {
             alert(
-                'Your comment could not be posted\nError: ' + (action.error ? action.error.message : 'Fetch failed')
+                'Your comment could not be posted\nError: ' + action.error ? action.error.message : 'Fetch failed'
             )
         }
     }
